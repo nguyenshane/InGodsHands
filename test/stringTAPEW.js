4605aa0bccf00d10a31908e291f93da7f6a5878b
Shane Do <nguyenshane@gmail.com>
2015-01-22 22:24:24 -0800

    translated arduino code into js

--- test/stringTAPEW.js
+++ test/stringTAPEW.js
@@ -13,8 +13,8 @@ pc.script.create('stringTAPEW', function (context) {
 
 
 
-        this.pinA = false;
-        this.pinB = false;
+        this.pinA = false; //S
+        this.pinB = false; //D
         this.pinAcache = false;
         this.pinBcache = false;
 
@@ -30,18 +30,65 @@ pc.script.create('stringTAPEW', function (context) {
 
         // Called every frame, dt is time in seconds since last update
         update: function (dt) {
+            /* FRAME DRIVING */
+
         },
 
-        /*
-        * Event handler called when key is pressed
-        */
+
+        /* EVENT DRIVING */
+        
+        doEncoderA: function (){
+            //console.log('called A');
+            // false->true on A
+            if(this.pinA){
+                // check direction on B
+                if(this.pinB == false) this.currentPos += 1; // CW
+                else this.currentPos -= 1; // CCW
+            }
+            else { // true->false on A
+                // check direction on B
+                if(this.pinB) this.currentPos += 1; // CW
+                else this.currentPos -= 1; // CCW
+            }
+            console.log(this.currentPos);
+        },
+
+        doEncoderB: function (){
+            //console.log("called B");
+            // false->true on B
+            if(this.pinB){
+                // check direction on A
+                if(this.pinA) this.currentPos += 1; // CW
+                else this.currentPos -= 1; // CCW
+            }
+            else { // true->false on A
+                // check direction on B
+                if(this.pinA == false) this.currentPos += 1; // CW
+                else this.currentPos -= 1; // CCW
+            }
+            console.log(this.currentPos);
+        },
+
+
+        // Event handler called when key is pressed
+        
         onKeyDown: function (event) {
             // Check event.key to detect which key has been pressed
             if (event.key === pc.KEY_S) {
-                console.log("S is pressing");
+                if(!this.pinA){
+                    this.pinA = true;
+                    this.doEncoderA();
+                    console.log("S is pressing");
+                }
+                
             }
             if (event.key === pc.KEY_D) {
-                console.log("D is pressing");
+                if(!this.pinB){
+                    this.pinB = true;
+                    this.doEncoderB();
+                    console.log("D is pressing");
+                }
+                
             }
 
             // When the space bar is pressed this scrolls the window.
@@ -49,18 +96,26 @@ pc.script.create('stringTAPEW', function (context) {
             event.event.preventDefault();
         },
 
-        /*
-        * Event handler called when key is released
-        */
+        
+        // Event handler called when key is released
+        
         onKeyUp: function (event) {
             // Check event.key to detect which key has been pressed
             if (event.key === pc.KEY_S) {
-                console.log("S is NOT pressing");
+                if(this.pinA){
+                    console.log("S is NOT pressing");
+                    this.pinA = false;
+                    this.doEncoderA();
+                }
             }
             if (event.key === pc.KEY_D) {
-                console.log("D is NOT pressing");
+                if(this.pinB){
+                    console.log("D is NOT pressing");
+                    this.pinB = false;
+                    this.doEncoderB();
+                }
             }
-        },
+        }, 
     };
 
     return stringTAPEW;
