9909f0237702d76e67dfb41a656408c365a83ba8
Matt Scorca <mscorca@ucsc.edu>
2015-02-12 21:33:01 -0800

    Editting for tribe class movement. Trying to set destination to a boundary rather than a hard position

--- AI/tribe.js
+++ AI/tribe.js
@@ -28,8 +28,7 @@ pc.script.create('tribe', function (context) {
             //this.currTileTemperature = this.tile.temperature;
             this.createRuleList();
             console.log(this.currTileTemperature);
-            this.destinationTile = ico.tiles[225];
-            console.log(!this.tile.equals(this.destinationTile));
+            this.destinationTile = ico.tiles[0];
             console.log("Current tile pos: " + this.entity.getPosition().toString() +  " dest tile pos: " + this.destinationTile.center.toString());
 
             // A shader definition used to create a new shader.
@@ -89,7 +88,11 @@ pc.script.create('tribe', function (context) {
         },
 
         // Called every frame, dt is time in seconds since last update
-        update: function (dt) { 
+        update: function (dt) {
+
+            // Rules system is run through each from, sorted by weight
+            // if the NPC is moving to another tile, moveTo is called instead
+
             this.rules.sort(function(a, b){return b.weight-a.weight});
             if(this.tile.equals(this.destinationTile)){
                 for(var i = 0; i < this.rules.length; i++){
@@ -100,11 +103,6 @@ pc.script.create('tribe', function (context) {
             } else {
                 this.moveTo();
             }
-            //this.moveRandom();
-            //this.entity.setPosition(ico.vertices[this.tile.vertexIndices[0] * 3 + 0], 
-            //                        ico.vertices[this.tile.vertexIndices[0] * 3 + 1], 
-            //                        ico.vertices[this.tile.vertexIndices[0] * 3 + 2]);
-
 
             // Set lighting in shader
             this.material.setParameter('sunDir', [sun.localRotation.x, sun.localRotation.y, sun.localRotation.z]);
@@ -112,63 +110,35 @@ pc.script.create('tribe', function (context) {
             //this.entity.setLocalEulerAngles(this.rotation.x, this.rotation.y, this.rotation.z);
             //this.entity.rotateLocal(90, 0, 0);
         },
-        
-        moveRandom: function() {
-            var rand = Math.random();
-            //console.log("Testing moveRandom()");
-            if (rand < .033) {
-                //console.log("Neighbora Position: (" + ico.vertices[this.tile.neighbora.vertexIndices[0] * 3 + 0] + ", " + ico.vertices[this.tile.neighbora.vertexIndices[0] * 3 + 1] + ", " + ico.vertices[this.tile.neighbora.vertexIndices[0] * 3 + 2] + ")");
-                this.tile = this.tile.neighbora;
-                this.rotation = this.tile.getRotationAlignedWithNormal();
-                //console.log(this.rotation);
-                this.entity.setLocalEulerAngles(this.rotation.x, this.rotation.y, this.rotation.z);
-                this.entity.rotateLocal(90, 0, 0);
 
-                //console.log(this.tile.normal);
-            } else if (rand < .066) {
-                //console.log("Neighborb Position: (" + ico.vertices[this.tile.neighborb.vertexIndices[0] * 3 + 0] + ", " + ico.vertices[this.tile.neighborb.vertexIndices[0] * 3 + 1] + ", " + ico.vertices[this.tile.neighborb.vertexIndices[0] * 3 + 2] + ")");
-                this.tile = this.tile.neighborb;
-                this.rotation = this.tile.getRotationAlignedWithNormal();
-                //console.log(this.rotation);
-                this.entity.setLocalEulerAngles(this.rotation.x, this.rotation.y, this.rotation.z);
-                this.entity.rotateLocal(90, 0, 0);
-
-                //console.log(this.tile.normal);
-            } else if (rand < .099) {
-                //console.log("Neighborc Position: (" + ico.vertices[this.tile.neighborc.vertexIndices[0] * 3 + 0] + ", " + ico.vertices[this.tile.neighborc.vertexIndices[0] * 3 + 1] + ", " + ico.vertices[this.tile.neighborc.vertexIndices[0] * 3 + 2] + ")");
-                this.tile = this.tile.neighborc;
-                this.rotation = this.tile.getRotationAlignedWithNormal();
-                //console.log(this.rotation);
-                this.entity.setLocalEulerAngles(this.rotation.x, this.rotation.y, this.rotation.z);
-                this.entity.rotateLocal(90, 0, 0);
-                
-                //console.log(this.tile.normal);
-            }
-        },
-
+        // Called every movement frame, lerps from one tile center to the next
         moveTo: function() {
-            //this.destinationTile = destinationTile;
-            //var destTilePos = new pc.Vec3(ico.vertices[this.tile.vertexIndices[0] * 3 + 0], 
-            //                              ico.vertices[this.tile.vertexIndices[0] * 3 + 1], 
-            //                              ico.vertices[this.tile.vertexIndices[0] * 3 + 2]);
-
-            //var lerpVec = new pc.Vec3();
-            //console.log(lerpVec.lerp(this.tile.center, this.destinationTile.center, .0001).toString());
-            deltaVec.lerp(this.entity.getPosition(), this.destinationTile.center, .0001);
+            deltaVec.lerp(this.entity.getPosition(), this.destinationTile.center, .1);
             this.entity.setPosition(deltaVec);   
-            //console.log(this.entity.getPosition().toString());
-            //console.log("tribe: " + this.entity.getPosition().toString());         
+            console.log("Curr pos: " + this.entity.getPosition().x);
+            console.log("dest pos: " + this.destinationTile.center.x);
 
-            //this.entity.setPosition(ico.vertices[this.tile.vertexIndices[0] * 3 + 0], 
-            //                        ico.vertices[this.tile.vertexIndices[0] * 3 + 1], 
-            //                        ico.vertices[this.tile.vertexIndices[0] * 3 + 2]);
-            if(this.entity.getPosition() === this.destinationTile.center){
+
+            // Once tribe is at next tile's center, movement is done.
+            if(this.atDestination()){
                 this.tile = this.destinationTile;
             }
         },
 
         setDestination: function(destination) {
             this.destinationTile = destination;
+        },
+
+        atDestination: function() {
+            if( this.entity.getPosition().x <= (this.destinationTile.center.x - .1) &&
+                this.entity.getPosition().y <= (this.destinationTile.center.y - .1) &&
+                this.entity.getPosition().z <= (this.destinationTile.center.z - .1) ){
+
+                this.entity.setPosition(this.destinationTile.center);
+                return true;
+            } else {
+                return false;
+            }
         },
 
         getPopulation: function() {
