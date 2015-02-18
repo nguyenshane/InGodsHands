6d7bd3fca828fac3a6a3f84635693d924fcb1f23
Shane Do <nguyenshane@gmail.com>
2015-02-17 17:44:24 -0800

    Basic Trees.js is done. Use makeTree(x,y,z) to make a tree

--- Environments/Trees.js
+++ Environments/Trees.js
@@ -15,8 +15,8 @@ pc.script.create('trees', function (context) {
     Trees.prototype = {
         // Called once after all resources are loaded and before the first update
         initialize: function () {
-            this.atm = context.root.findByName("Atmosphere");
-            this.atmstack = [];
+            this.trees = context.root.findByName("Trees");
+            this.trees_stack = [];
         },
 
         // Called every frame, dt is time in seconds since last update
@@ -24,54 +24,44 @@ pc.script.create('trees', function (context) {
             var x = Math.floor((Math.random() * 360) + 0);
             var z = Math.floor((Math.random() * 360) + 0);
             
-            if (this.atmstack.length < this.stackBuffer ){
-                this.makeFog(x,0,z);
-                this.makeRain(x,0,z);
+            if (this.trees_stack.length < this.stackBuffer ){
+                this.makeTree(x,0,z);
             }
             
             
-            if (this.atmstack.length >= this.stackBuffer){
-                var e = this.atmstack.shift();
+            if (this.trees_stack.length >= this.stackBuffer){
+                var e = this.trees_stack.shift();
                 var destroyable = this.checkDestroyable(e);
                 
                 if (destroyable) e.destroy();
-                else this.atmstack.unshift(e)
+                else this.trees_stack.unshift(e)
             }
             
-            //console.log(this.atmstack.length);
         },
         
-        makeRain: function(x,y,z) {
-            var e = this.atm.clone(); // Clone Atmosphere
+        makeTree: function(x,y,z) {
+            var e = this.trees.clone(); // Clone Atmosphere
             
             this.entity.getParent().addChild(e); // Add it as a sibling to the original
             
             e.rotateLocal(x, y, z);
-            e.findByName("RainPS").particlesystem.enabled = true;
-            e.findByName("RainPS").particlesystem.play();
+
+            var x = Math.floor((Math.random() * 360) + 0);
+            var z = Math.floor((Math.random() * 360) + 0);
             
-            this.atmstack.push(e);
+            var treetype = Math.floor((Math.random() * 2) + 0);
+            console.log("treetype", treetype)
+
+            if(treetype == 0)
+                e.findByName("tree1").enabled = true;
+            else e.findByName("tree2").enabled = true;
+            
+            
+            this.trees_stack.push(e);
         },
         
-        makeFog: function(x,y,z) {
-            var e = this.atm.clone(); // Clone Atmosphere
-            
-            this.entity.getParent().addChild(e); // Add it as a sibling to the original
-            
-            e.rotateLocal(x, y, z);
-            e.findByName("FogPS").particlesystem.enabled = true;
-            e.findByName("FogPS").particlesystem.play();
-            
-            this.atmstack.push(e);
-        },
         
-        checkDestroyable: function(e) {
-            if (e.findByName("RainPS").particlesystem.enabled && !e.findByName("RainPS").particlesystem.isPlaying())
-                return true;
-            
-            if (e.findByName("FogPS").particlesystem.enabled && !e.findByName("FogPS").particlesystem.isPlaying())
-                return true;
-            
+        checkDestroyable: function(e) {            
             return false;
             
         },
