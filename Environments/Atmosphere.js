f4d9853e70696ae62774f4efaac63ee67e100ce2
Shane Do <nguyenshane@gmail.com>
2015-03-03 21:45:47 -0800

    Updated the reuse pool for Particle System, should work now

--- Environments/Atmosphere.js
+++ Environments/Atmosphere.js
@@ -21,6 +21,9 @@ pc.script.create('Atmosphere', function (context) {
 
         // Called every frame, dt is time in seconds since last update
         update: function (dt) {
+            
+            //console.log('fogstack', this.fogstack.length);
+            //console.log('rainstack', this.rainstack.length);
 			/*
             if (this.rainstack.length < this.stackBuffer) {
                 this.makeFog(x,0,z);
@@ -59,11 +62,12 @@ pc.script.create('Atmosphere', function (context) {
         
         makeRain: function(position, rotation) {
             if(this.rainstack.length < this.stackBuffer){
+
                 var e = this.atm.clone(); // Clone Atmosphere
                 
                 this.entity.getParent().addChild(e); // Add it as a sibling to the original
     			
-    			e.destroyable = false;
+    			//e.destroyable = false;
     			
     			var rain = e.findByName("RainPS");
                 
@@ -79,12 +83,13 @@ pc.script.create('Atmosphere', function (context) {
                 for (var i = 0; i < this.rainstack.length; i++) {
                     var e = this.rainstack.shift();
                     if (this.checkDestroyable(e)) {
-                        //e.destroy();
                         // if this is reusable
-                        e.rotate(rotation.x - 90, rotation.y, rotation.z);
-                        e.setPosition(position);
-                        e.particlesystem.enabled = true;
-                        e.particlesystem.play();
+                        var rain = e.findByName("RainPS");
+                        //rain.rotate(rotation.x - 90, rotation.y, rotation.z);
+                        rain.setPosition(position);
+
+                        rain.particlesystem.reset();
+                        rain.particlesystem.play();
                         return e;
                     } else {
                         this.rainstack.push(e);
@@ -99,7 +104,7 @@ pc.script.create('Atmosphere', function (context) {
                 
                 this.entity.getParent().addChild(e); // Add it as a sibling to the original
     			
-    			e.destroyable = false;
+    			//e.destroyable = false;
                 
     			var fog = e.findByName("FogPS");
                 
@@ -112,28 +117,31 @@ pc.script.create('Atmosphere', function (context) {
                 this.fogstack.push(e);
     			return e;
             } else {
+                
                 for (var i = 0; i < this.fogstack.length; i++) {
                     var e = this.fogstack.shift();
                     if (this.checkDestroyable(e)) {
-                        //e.destroy();
                         // if this is reusable
-                        e.rotate(rotation.x - 90, rotation.y, rotation.z);
-                        e.setPosition(position);
-                        e.particlesystem.enabled = true;
-                        e.particlesystem.play();
+                        var fog = e.findByName("FogPS");
+                        fog.rotate(rotation.x - 90, rotation.y, rotation.z);
+                        fog.setPosition(position);
+
+                        fog.particlesystem.reset();
+                        fog.particlesystem.play();
                         return e;
                     } else {
-                        this.rainstack.push(e);
+                        this.fogstack.push(e);
                     }
                 }
             }
         },
         
         checkDestroyable: function(e) {
-			if (!e.destroyable) return false;
+			//if (!e.destroyable) return false;
 			
             if (e.findByName("RainPS").particlesystem.enabled && !e.findByName("RainPS").particlesystem.isPlaying())
                 return true;
+            
             
             if (e.findByName("FogPS").particlesystem.enabled && !e.findByName("FogPS").particlesystem.isPlaying())
                 return true;
