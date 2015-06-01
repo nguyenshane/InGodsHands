f441d58effdb5ce5013d6c49a4b297a3afab9214
Shane Do <nguyenshane@gmail.com>
2015-06-01 01:19:59 -0700

    Testing new load scene

--- Intro/game.js
+++ Intro/game.js
@@ -3,59 +3,46 @@ pc.script.create('game', function (context) {
     var Game = function (entity) {
         this.entity = entity;
         
-        this.LEVELS = [
-            365042, // intro
-            350057 // Rv1-stable
+        this.ROOTS = [
+            '365042.json', // intro
+            '350057.json' // Rv1-stable
         ];
+
+        this.currentRoot = null;
     };
 
     Game.prototype = {
         // Called once after all resources are loaded and before the first update
         initialize: function () {
-            this.currentPack = null;
-            this.loadLevel(1);
+            this.loadRoot(1);
         },
 
         // Called every frame, dt is time in seconds since last update
         update: function (dt) {
         },
         
-        completeLevel: function (level) {
-            this.unloadLevel(level-1);
+        loadNextRoot: function (level) {
+            this.unloadRoot(level-1);
             if (level === this.LEVELS.length) {
                 level = 0;
             }
-            this.loadLevel(level);
+            this.loadRoot(level);
+        },
+
+        reset: function () {
+            this.unloadRoot();
+            this.loadRoot(0);
         },
         
-        unloadLevel: function () {
+        unloadRoot: function () {
             // Destroy all Entities and components created from the previous Pack.
-            console.log('currentPack to destroy', this.currentPack);
-            this.currentPack.hierarchy.destroy();
-            this.currentPack = null;
+            console.log('currentRoot to destroy', this.currentRoot);
+            this.currentRoot.destroy();
+            this.currentRoot = null;
         },
         
-        loadLevel: function (index) {
-            var self = this;
-            var pack = this.LEVELS[index];
-    
-            // Create a request for a Pack, using the Pack GUID as the identifier.
-            var request = new pc.resources.PackRequest(pack);
-            console.log('request',request);
-            // Make the request through the Resource Loader
-            context.loader.request(request).then(function (resources) {
-                // resources[0] is a loaded pc.fw.Pack object
-                var pack = resources[0];
-                
-                self.currentPack = pack;
-                
-                // pack.hierarchy is the Entity hierarchy, you need to add this to the main hierarchy
-                context.root.addChild(pack.hierarchy);
-                
-                // This should be done for you, but for now you need to 
-                // initialise any systems with an initialize() method after pack is loaded
-                pc.fw.ComponentSystem.initialize(pack.hierarchy);
-            });
+        loadRoot: function (index) {
+            console.log("scene", context.loadSceneHierarchy(this.ROOTS[index]));
         }
     };
 
