4a1de3087d8edc35d5a3037b64ccafbc3170dbe0
Shane Do <nguyenshane@gmail.com>
2015-05-31 21:05:20 -0700

    Making sure not conflicting with Chase

--- HIDInterface/SwipeInterface.js
+++ HIDInterface/SwipeInterface.js
@@ -63,39 +63,6 @@ pc.script.create('SwipeInterface', function (context) {
                     box-shadow: none;
                     position: relative;
                 }
-                .center{
-                    height:20%;
-                    width: 40%;
-                    background: none;
-                    float:left;
-                }
-                .right{
-                    height:20%;
-                    width: 30%;
-                    background: none;
-                    float:left;
-                    position: relative;
-                }
-                .up{ opacity: 1 }
-
-                .T_L{ background: linear-gradient(to right, rgba(63,64,63,1) 40%, rgba(0,0,0,0) 100%); 
-                        transition: all 1.0s ease;}
-                
-                .T_L_active{ 
-                      transition: box-shadow 1.0s ease;
-                      background: rgba(48,76,80,1); box-shadow: 0 0 2em red;}
-
-                #A_L{ background: linear-gradient(to right, rgba(126,127,125,1) 30%, rgba(0,0,0,0) 100%); }
-                #P_L{ background: linear-gradient(to right, rgba(188,191,188,1) 20%, rgba(0,0,0,0) 100%); }
-                #E_L{ background: linear-gradient(to right, rgba(126,127,125,1) 30%, rgba(0,0,0,0) 100%); }
-                #W_L{ background: linear-gradient(to right, rgba(63,64,63,1) 40%, rgba(0,0,0,0) 100%); }
-
-                #T_R{ background: linear-gradient(to right, rgba(0,0,0,0) 0%, rgba(63,64,63,1) 60%); }
-                #A_R{ background: linear-gradient(to right, rgba(0,0,0,0) 0%, rgba(126,127,125,1) 70%); }
-                #P_R{ background: linear-gradient(to right, rgba(0,0,0,0) 0%, rgba(188,191,188,1) 80%); }
-                #E_R{ background: linear-gradient(to right, rgba(0,0,0,0) 0%, rgba(126,127,125,1) 70%); }
-                #W_R{ background: linear-gradient(to right, rgba(0,0,0,0) 0%, rgba(63,64,63,1) 60%); }
-
                 .left img {
                     height: 50%;
                     top: 25%;
@@ -110,7 +77,19 @@ pc.script.create('SwipeInterface', function (context) {
                     opacity: 0;
                     transition: all 0.8s ease;
                 }
-
+                .center{
+                    height:20%;
+                    width: 40%;
+                    background: none;
+                    float:left;
+                }
+                .right{
+                    height:20%;
+                    width: 30%;
+                    background: none;
+                    float:left;
+                    position: relative;
+                }
                 .right img {
                     height: 50%;
                     top: 25%;
@@ -119,6 +98,38 @@ pc.script.create('SwipeInterface', function (context) {
                     pointer-events: none;
                     float: right;
                 }
+
+                .right img.active{
+                    opacity: 0;
+                    transition: all 0.8s ease;
+                }
+
+                .T_L{ background: linear-gradient(to right, rgba(63,64,63,1) 40%, rgba(0,0,0,0) 100%); 
+                        transition: all 1.0s ease;}
+                
+                .T_L_active{ 
+                      transition: box-shadow 1.0s ease;
+                      background: rgba(48,76,80,1); box-shadow: 0 0 2em white;}
+
+                #A_L{ background: linear-gradient(to right, rgba(126,127,125,1) 30%, rgba(0,0,0,0) 100%); }
+                #P_L{ background: linear-gradient(to right, rgba(188,191,188,1) 20%, rgba(0,0,0,0) 100%); }
+                #E_L{ background: linear-gradient(to right, rgba(126,127,125,1) 30%, rgba(0,0,0,0) 100%); }
+                #W_L{ background: linear-gradient(to right, rgba(63,64,63,1) 40%, rgba(0,0,0,0) 100%); }
+
+                .T_R{ background: linear-gradient(to right, rgba(0,0,0,0) 0%, rgba(63,64,63,1) 60%); }
+
+                .T_R_active{ 
+                      transition: box-shadow 1.0s ease;
+                      background: rgba(48,76,80,1); box-shadow: 0 0 2em white;}
+
+                #A_R{ background: linear-gradient(to right, rgba(0,0,0,0) 0%, rgba(126,127,125,1) 70%); }
+                #P_R{ background: linear-gradient(to right, rgba(0,0,0,0) 0%, rgba(188,191,188,1) 80%); }
+                #E_R{ background: linear-gradient(to right, rgba(0,0,0,0) 0%, rgba(126,127,125,1) 70%); }
+                #W_R{ background: linear-gradient(to right, rgba(0,0,0,0) 0%, rgba(63,64,63,1) 60%); }
+
+                
+
+                
             */}.toString().trim();
             css = css.slice(css.indexOf('/*') + 2).slice(0, -3);
 
@@ -138,10 +149,14 @@ pc.script.create('SwipeInterface', function (context) {
             var swipeWrapper = document.createElement('div');
             swipeWrapper.className = 'swipeWrapper';
             swipeWrapper.innerHTML = [
-                '<div class="left T_L" id="T_L"><img id="TLidle" src="'+TLidle+'"/><img id="TLactive" class="active" src="'+TLactive+'"/></div><div class="center"></div> <div class="right T_R" id="T_R"><img src="'+TRidle+'"/></div>',
+                '<div class="left T_L" id="T_L"><img id="TLidle" src="'+TLidle+'"/><img id="TLactive" class="active" src="'+TLactive+'"/></div><div class="center"></div><div class="right T_R" id="T_R"><img id="TRidle" src="'+TRidle+'"/><img id="TRactive" class="active" src="'+TRactive+'"/></div>',
+
                 '<div class="left A_L" id="A_L"><img src="'+ALidle+'"/></div> <div class="center"></div> <div class="right A_R" id="A_R"><img src="'+ARidle+'"/></div>',
+
                 '<div class="left P_L" id="P_L"><img src="'+PLidle+'"/></div> <div class="center"></div> <div class="right P_R" id="P_R"><img src="'+PRidle+'"/></div>',
+
                 '<div class="left E_L" id="E_L"><img src="'+ELidle+'"/></div> <div class="center"></div> <div class="right E_R" id="E_R"><img src="'+ERidle+'"/></div>',
+
                 '<div class="left W_L" id="W_L"><img src="'+WLidle+'"/></div> <div class="center"></div> <div class="right W_R" id="W_R"><img src="'+WRidle+'"/></div>'
                 ].join('\n');
             document.body.appendChild(swipeWrapper);
@@ -187,12 +202,30 @@ pc.script.create('SwipeInterface', function (context) {
             });
 
             this.T_R.swipe({
+                tap:function(event, target) {
+                  console.log("tap event ", event, " target ", target);
+                  var jtarget = $(target);
+                  jtarget.children(".active").css('opacity', 0.8);
+
+                  jtarget.addClass('T_R_active');
+                  console.log("jtarget.children ", jtarget.children(".active"));
+                },
+                hold:function(event, target) {
+                  //console.log("tap event ", event, " target ", target);
+                  var jtarget = $(target);
+                  jtarget.children(".active").css('opacity', 0.8);
+                  jtarget.addClass('T_R_active');
+                },
                 //Generic swipe handler for all directions
                 swipeRight:function(event, direction, distance, duration, fingerCount, fingerData) {
                   //console.log("You swiped T_R" + " distance " + distance, " duration " + duration);
                   HIDInterface.moved_T(1,distance/50,(distance/50)/duration);
+                  var jtarget = $(event.srcElement);
+                  jtarget.children(".active").css('opacity', 0);
+                  jtarget.removeClass('T_R_active');
                 },
-                threshold:0
+                //threshold:0
+                longTapThreshold:0
             });
 
             this.A_L.swipe({
